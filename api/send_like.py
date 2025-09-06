c198ebfc74e8df7fe3c2dc464b1651e69c2e873c
msryl7956-blip <msryl7956@gmail.com>
2025-09-06 03:16:17 +0100

    Update send_like.py

--- api/send_like.py
+++ api/send_like.py
@@ -128,13 +128,13 @@ def send_like():
     failed = []
 
     # حلقة مستمرة حتى نصل 100 لايك ناجح
-    while likes_sent < 100:
+    while likes_sent < 150:
         try:
             token_data = httpx.get("https://aauto-token.onrender.com/api/get_jwt", timeout=50).json()
             tokens_dict = token_data.get("tokens", {})
             token_items = list(tokens_dict.items())
             random.shuffle(token_items)
-            token_items = token_items[:150]  # 100 توكن جديدة في كل دورة
+            token_items = token_items[:250]  # 100 توكن جديدة في كل دورة
         except Exception as e:
             return jsonify({"error": f"Failed to fetch tokens: {e}"}), 500
 
