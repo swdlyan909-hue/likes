17d2ceb382456e9d51a7209af9186eabfd824b2c
swdlyan909-hue <swdlyan909@gmail.com>
2025-09-13 19:44:02 +0100

    Update send_like.py

--- api/send_like.py
+++ api/send_like.py
@@ -105,7 +105,7 @@ def send_like():
 
     # جلب معلومات اللاعب قبل الإرسال
     try:
-        info_url = f"https://info-plum-six.vercel.app/get?uid={player_id}"
+        info_url = f"https://info-five-sooty.vercel.app/get?uid={player_id}"
         resp = httpx.get(info_url, timeout=10)
         info_json = resp.json()
         account_info = info_json.get("AccountInfo", {})
